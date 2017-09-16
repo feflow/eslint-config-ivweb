437b47d8af6edff5c67f5ab0a63646b67d420a5b
cpselvis <cpselvis@gmail.com>
2017-09-16 20:35:50 +0800

    feat: modify eslint:recommend rules and add new rules

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 
 module.exports = {
-    extends: 'airbnb-base',
     rules: {
         // getter必须有返回值，并且禁止返回值为undefined, 比如 return;
-        'getter-return': ['error', { allowImplicit: false }]
+        'getter-return': [2, { allowImplicit: false }]
     }
 };
