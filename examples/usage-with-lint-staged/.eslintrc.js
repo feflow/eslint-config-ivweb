7d3af5f2ad1a501c19cfdb3de7c7c337b12c43e1
Lewis Cheng <cpselvis@gmail.com>
2017-10-09 17:10:25 +0800

    docs: update example .eslintrc.js global variables

--- examples/usage-with-lint-staged/.eslintrc.js
+++ examples/usage-with-lint-staged/.eslintrc.js
@@ -4,5 +4,10 @@ module.exports = {
         "browser": true,
         "node": true
     },
-    "extends": ["eslint:recommended", "ivweb"]
+    "extends": ["eslint:recommended", "ivweb"],
+    "globals": {
+        "__inline": true,
+        "IS_SERVER": true,
+        "__uri": true
+    }
 };
