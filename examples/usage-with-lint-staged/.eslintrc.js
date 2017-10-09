708f7c61aba20622ae0a44bddc57c8b1db8c90f3
cpselvis <cpselvis@gmail.com>
2017-10-09 16:44:56 +0800

    docs: modify example name

---
+++ examples/usage-with-lint-staged/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true,
+        "node": true
+    },
+    "extends": ["eslint:recommended", "ivweb"]
+};
---
+++ examples/usage-with-lint-staged/index.js
@@ -0,0 +1,12 @@
+var a = 1,
+    b = 2;
+
+var c = a + b;
+var d = a + b;
+
+console.log(c);
+console.log(d);
+
+alert(111);
+
+alert(222);
